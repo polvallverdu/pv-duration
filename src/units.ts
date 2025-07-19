86b93dd157f73295b9b43dce46b6b630f3ec0c55
polvallverdu <pvallverdualcaide@gmail.com>
2025-07-19 12:05:17 +0200

    fix: everything

--- src/units.ts
+++ src/units.ts
@@ -7,34 +7,34 @@ export type DurationWithUnit = `${number}${Unit}` | `${number} ${Unit}`;
  * @returns The duration in milliseconds
  */
 export const parseDuration = (duration: DurationWithUnit) => {
-	const match = duration.match(/^(\d+)\s*([a-zA-Z]+)$/);
-	if (!match) {
-		throw new Error(`Invalid duration format: ${duration}`);
-	}
-	const [, value, unit] = match;
+  const match = duration.match(/^(\d+)\s*([a-zA-Z]+)$/);
+  if (!match) {
+    throw new Error(`Invalid duration format: ${duration}`);
+  }
+  const [, value, unit] = match;
 
-	const valueNumber = Number(value);
+  const valueNumber = Number(value);
 
-	if (Number.isNaN(valueNumber)) {
-		throw new Error(`Invalid duration format: ${duration}`);
-	}
+  if (Number.isNaN(valueNumber)) {
+    throw new Error(`Invalid duration format: ${duration}`);
+  }
 
-	switch (unit) {
-		case "ms":
-			return valueNumber * 1;
-		case "s":
-			return valueNumber * 1000;
-		case "m":
-			return valueNumber * 1000 * 60;
-		case "h":
-			return valueNumber * 1000 * 60 * 60;
-		case "d":
-			return valueNumber * 1000 * 60 * 60 * 24;
-		case "w":
-			return valueNumber * 1000 * 60 * 60 * 24 * 7;
-		case "y":
-			return valueNumber * 1000 * 60 * 60 * 24 * 365;
-		default:
-			throw new Error(`Invalid duration format: ${duration}`);
-	}
+  switch (unit) {
+    case "ms":
+      return valueNumber * 1;
+    case "s":
+      return valueNumber * 1000;
+    case "m":
+      return valueNumber * 1000 * 60;
+    case "h":
+      return valueNumber * 1000 * 60 * 60;
+    case "d":
+      return valueNumber * 1000 * 60 * 60 * 24;
+    case "w":
+      return valueNumber * 1000 * 60 * 60 * 24 * 7;
+    case "y":
+      return valueNumber * 1000 * 60 * 60 * 24 * 365;
+    default:
+      throw new Error(`Invalid duration format: ${duration}`);
+  }
 };
